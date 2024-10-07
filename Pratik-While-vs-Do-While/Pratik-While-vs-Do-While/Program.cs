ead3cd54a7059902121d04efe6d7a6a583b2b61c
OrkunDemir5 <orkun.d1999@gmail.com>
2024-10-08 00:59:00 +0300

    Pratik-While-vs-Do-While

---
+++ Pratik-While-vs-Do-While/Pratik-While-vs-Do-While/Program.cs
@@ -0,0 +1,26 @@
+﻿
+int limit = int.Parse(Console.ReadLine());
+int sayac = 0;
+
+/////////////// Do - While ///////////////
+
+do
+{
+    Console.WriteLine("Ben bir Patika'lıyım");
+    sayac++;
+
+}while(sayac <= limit);
+
+/////////////// While ///////////////
+
+while (sayac <= limit)
+{
+    Console.WriteLine();
+    sayac++;
+}
+
+// while döngüsünde koşul baştan kontrol edilir. Eğer başlangıç koşulu sağlanmıyorsa döngüye hiç girilmez.
+// Örneğin, limit -5 olduğunda while döngüsü hiç çalışmaz.
+
+// do-while döngüsünde ise koşul döngünün sonunda kontrol edilir. Bu nedenle, koşul yanlış olsa bile döngü en az bir kez çalışır.
+// Örneğin, limit -5 olsa bile do-while döngüsü bir kez çalışır ve ekrana "Ben bir Patika'lıyım" yazdırır.
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Pratik-While-vs-Do-While
- 
+ ## Hem while hem de do-while dongusu kullanarak kullanicidan bir limit degeri alip, 0'dan baslayarak "Ben bir Patika'liyim" mesajini yazdiran C# kodlari bulunmaktadir.
+
+ While ve do-while arasindaki farki gosterdim. 
